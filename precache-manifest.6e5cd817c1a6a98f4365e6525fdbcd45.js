260989d5440afe1ef9a3b2d8ccb3cc6a5cf8c217
Shujaat Iqbal <shojaatiqbal@gmail.com>
2020-06-03 22:36:27 +0500

    Updates

---
+++ precache-manifest.6e5cd817c1a6a98f4365e6525fdbcd45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d418a950620029f41d1e968f5c58acc",
+    "url": "/react-demo-app/index.html"
+  },
+  {
+    "revision": "9b756098f8bff2787cac",
+    "url": "/react-demo-app/static/css/main.8f5114eb.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/react-demo-app/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo-app/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b756098f8bff2787cac",
+    "url": "/react-demo-app/static/js/main.48b0c4b8.chunk.js"
+  },
+  {
+    "revision": "eb47194167a1bdfd6ad5",
+    "url": "/react-demo-app/static/js/runtime-main.6346c9c8.js"
+  }
+]);
